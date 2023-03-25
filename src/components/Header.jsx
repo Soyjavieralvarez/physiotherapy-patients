8487eae8c105b4e032aa8dd3eefc206568c2a41a
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-03-25 18:32:27 +0100

    finished project!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
     <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'> 
-    Seguimiento de pacientes {''} con GitHub {''}
+    Seguimiento de pacientes {''} 
     <span className="text-teal-500 ">Fisioterapia</span>
     </h1>
     </>
