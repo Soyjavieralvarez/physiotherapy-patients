51377e3b990ce66b5c40a7b59d7a7bc7c87757a2
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-03-25 13:47:13 +0100

    local storage done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
-import { useState } from 'react' 
-import Header from "./components/Header"
-import Form from "./components/Form"
-import PatientList from "./components/PatientList"
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import PatientList from "./components/PatientList";
 
 function App() {
-
   const [patients, setPatients] = useState([]);
   const [patient, setPatient] = useState({});
-  const deletedPatient = id => {
-    const patientsUpdate = patients.filter( patient => patient.id !== id);
-    setPatients(patientsUpdate)
-  }
 
+  useEffect(() => {
+    const patientsLocal = JSON.parse(localStorage.getItem('patients'));
+    patientsLocal?.length > 0 && setPatients(patientsLocal)
+  }, []);
 
- 
+  useEffect(() => {
+    localStorage.setItem('patients', JSON.stringify(patients));
+  }, [patients]);
+
+  const deletedPatient = (id) => {
+    const patientsUpdate = patients.filter((patient) => patient.id !== id);
+    setPatients(patientsUpdate);
+  };
+
   return (
     <div className="container mx-auto mt-20">
-      <Header
-       />
+      <Header />
       <div className="mt-12 md:flex">
-      <Form
-      patients={patients}
-       setPatients={setPatients}
-       patient={patient}
-       setPatient={setPatient}
-       />
-      <PatientList
-      patients={patients}
-      setPatient={setPatient}
-      deletedPatient={deletedPatient}
-       />
+        <Form
+          patients={patients}
+          setPatients={setPatients}
+          patient={patient}
+          setPatient={setPatient}
+        />
+        <PatientList
+          patients={patients}
+          setPatient={setPatient}
+          deletedPatient={deletedPatient}
+        />
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
